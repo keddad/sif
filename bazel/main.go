9860dcec12844126ab51868eeac5ce6b8306d98e
keddad <keddad@yandex.ru>
2023-01-10 21:45:57 +0300

    MVP

--- bazel/main.go
+++ bazel/main.go
@@ -6,10 +6,6 @@ import (
 	"path/filepath"
 )
 
-type Target struct {
-	label, rel_label string
-}
-
 func BuildTarget(label string, workspacePath string, envArgs []string, verbose bool) error {
 	cmdArgs := []string{"build", label}
 	cmdArgs = append(cmdArgs, envArgs...)
@@ -19,19 +15,10 @@ func BuildTarget(label string, workspacePath string, envArgs []string, verbose b
 
 	out, err := cmd.CombinedOutput()
 
-	if err != nil {
-		log.Printf("Cannot build target %s\n", label)
-		log.Println(string(out))
-	}
-
 	if verbose {
 		log.Printf("Building %s in workspace %s", label, workspacePath)
 		log.Println(string(out))
 	}
 
 	return err
-}
-
-func ParseDeps(label string, workspacePath string, envArgs []string, verbose bool) ([]Target, error) {
-	return nil, nil
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/bazelbuild/buildtools/edit"
 	"github.com/keddad/sif/bazel"
 	"io/ioutil"
@@ -12,6 +13,7 @@ func main() {
 	label := flag.String("label", "not-specified-lol", "Label of target to cleanup")
 	workspacePath := flag.String("workspace", ".", "Workspace path")
 	verboseFlag := flag.Bool("v", false, "")
+	param := "deps"
 
 	bazelArgs := flag.Args()
 	flag.Parse()
@@ -32,11 +34,62 @@ func main() {
 		panic(err)
 	}
 
-	depsList, err := bazel.ExtractEntriesFromFile(content, target, "deps")
+	depsList, err := bazel.ExtractEntriesFromFile(content, target, param)
 
-	print(depsList)
+	fmt.Printf("Working with %s in workspace %s, optimizing param %s. Analyzing those options:\n", *label, *workspacePath, param)
 
-	// Build target with Bazel
-	// This populates the cache to make further operations faster, and ensures target builds before changes
-	bazel.BuildTarget(*label, *workspacePath, bazelArgs, *verboseFlag)
+	for _, elem := range depsList {
+		println(elem)
+	}
+
+	err = bazel.BuildTarget(*label, *workspacePath, bazelArgs, *verboseFlag)
+
+	if err != nil {
+		log.Fatalf("Can't build target with it's current deps!; %e", err)
+	}
+
+	removed := make([]string, 0)
+
+	for _, elem := range depsList {
+		if *verboseFlag {
+			log.Printf("Trying to remove %s", elem)
+		}
+
+		contentWithoutElem, err := bazel.DeleteEntryFromFile(content, target, param, elem)
+
+		if err != nil {
+			log.Fatalf("Can't remove %s from file; %e", elem, err)
+		}
+
+		err = ioutil.WriteFile(buildFile, contentWithoutElem, 0)
+
+		if err != nil {
+			log.Fatalf("Can't write BUILD file; %e", err)
+		}
+
+		err = bazel.BuildTarget(*label, *workspacePath, bazelArgs, *verboseFlag)
+
+		if err == nil {
+			log.Printf("%s dep is redundant, removing it", elem)
+			content = contentWithoutElem
+			removed = append(removed, elem)
+		} else if *verboseFlag {
+			log.Printf("%s dep is not redundant", elem)
+		}
+	}
+
+	err = ioutil.WriteFile(buildFile, content, 0)
+
+	if err != nil {
+		log.Fatalf("Can't write BUILD file; %e", err)
+	}
+
+	if len(removed) != 0 {
+		log.Print("Removed following dependencies:")
+		for _, elem := range removed {
+			println(elem)
+		}
+	} else {
+		log.Print("Removed no dependencies.")
+	}
 }
